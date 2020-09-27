5f05f3c5fe76fd618d167882ceac8fae4493ac6b
Faaez Razeen <razeenfaaez@gmail.com>
2020-09-28 00:06:57 +0530

    Sorting Visualizer v1

--- sorting-visualizer/js/sorting-algorithms.js
+++ sorting-visualizer/js/sorting-algorithms.js
@@ -3,22 +3,27 @@ function sleep(ms) {
 }
 
 function swap(x, y) {
-    const temp = bar_heights[x];
+    const temp1 = bar_heights[x];
     bar_heights[x] = bar_heights[y];
-    bar_heights[y] = temp;
+    bar_heights[y] = temp1;
+    const temp2 = bar_colors[x];
+    bar_colors[x] = bar_colors[y];
+    bar_colors[y] = temp2;
 }
 
 function apply_colors(initial_idx, limit_idx, bars_to_fill, color, algorithm) {
-    if (algorithm === 'insertion') {
-        for (let i = initial_idx; i >= initial_idx - bars_to_fill; i--) {
-            bar_colors[i] = color;
-        }
-    } else {
-        for (let i = initial_idx; i < initial_idx + bars_to_fill; i++) {
-            if (i < limit_idx) {
+    if (!colored_bars) {
+        if (algorithm === 'insertion') {
+            for (let i = initial_idx; i >= initial_idx - bars_to_fill; i--) {
                 bar_colors[i] = color;
-            } else {
-                break;
+            }
+        } else {
+            for (let i = initial_idx; i < initial_idx + bars_to_fill; i++) {
+                if (i < limit_idx) {
+                    bar_colors[i] = color;
+                } else {
+                    break;
+                }
             }
         }
     }
@@ -42,6 +47,37 @@ async function bubble_sort() {
     }
 }
 
+async function cocktail_shaker_sort() {
+    let lower_limit = 0;
+    let upper_limit = num_bars - 1;
+    while (lower_limit < upper_limit) {
+        let swapped_1 = false;
+        let swapped_2 = false;
+        for (let i = lower_limit; i < upper_limit; i++) {
+            if (bar_heights[i] > bar_heights[i + 1]) {
+                swap(i, i + 1);
+                swapped_1 = true;
+            }
+            apply_colors(i, num_bars, bar_color_width, 'red', 'bubble');
+            await sleep(4);
+            apply_colors(i, num_bars, bar_color_width, 'white', 'bubble');
+        }
+        upper_limit--;
+        for (let i = upper_limit; i > lower_limit; i--) {
+            if (bar_heights[i] < bar_heights[i - 1]) {
+                swap(i, i - 1);
+                swapped_2 = true;
+            }
+            apply_colors(i, num_bars, bar_color_width, 'red', 'bubble');
+            await sleep(4);
+            apply_colors(i, num_bars, bar_color_width, 'white', 'bubble');
+        }
+        lower_limit++;
+        if (!swapped_1 && !swapped_2)
+            break;
+    }
+}
+
 async function selection_sort() {
     for (let i = 0; i < num_bars; i++) {
         let min_idx = i;
@@ -61,42 +97,45 @@ async function insertion_sort() {
     for (let i = 1; i < num_bars; i++) {
         let current = i;
         let value = bar_heights[i];
+        let value_color = bar_colors[i];
         while (current > 0 && bar_heights[current - 1] > value) {
             apply_colors(current, 0, bar_color_width, 'red', 'insertion');
             bar_heights[current] = bar_heights[current - 1];
+            bar_colors[current] = bar_colors[current - 1];
             await sleep(4);
             apply_colors(current, 0, bar_color_width, 'white', 'insertion');
             current--;
         }
         bar_heights[current] = value;
+        bar_colors[current] = value_color;
     }
 }
 
-async function partition_lomuto(start, end) {
-    let pivot = bar_heights[end];
-    let partn_idx = start;
-    for (let i = start; i < end; i++) {
-        if (bar_heights[i] <= pivot) {
-            apply_colors(i, end, bar_color_width, 'red', 'quick_l');
-            await sleep(20);
-            swap(i, partn_idx);
-            partn_idx++;
-            apply_colors(i, end, bar_color_width, 'white', 'quick_l');
+async function shell_sort() {
+    let gap = Math.floor(num_bars / 2);
+    while (gap > 0) {
+        for (let i = gap; i < num_bars; i++) {
+            let j = i;
+            let temp = bar_heights[i];
+            let temp_color = bar_colors[i];
+            while (j >= gap && bar_heights[j - gap] > temp) {
+                apply_colors(j, gap, bar_color_width, 'red', 'insertion');
+                apply_colors(j - gap, gap, bar_color_width, 'red', 'insertion');
+                bar_heights[j] = bar_heights[j - gap];
+                bar_colors[j] = bar_colors[j - gap];
+                await sleep(4);
+                apply_colors(j, gap, bar_color_width, 'white', 'insertion');
+                apply_colors(j - gap, gap, bar_color_width, 'white', 'insertion');
+                j -= gap;
+            }
+            bar_heights[j] = temp;
+            bar_colors[j] = temp_color;
         }
-    }
-    swap(partn_idx, end);
-    return partn_idx;
-}
-
-async function quick_sort_lomuto(start = 0, end = num_bars - 1) {
-    if (start < end) {
-        let partn_idx = await partition_lomuto(start, end);
-        await quick_sort_lomuto(start, partn_idx - 1);
-        await quick_sort_lomuto(partn_idx + 1, end);
+        gap = Math.floor(gap / 2);
     }
 }
 
-async function partition_hoare(start, end) {
+async function partition(start, end) {
     let pivot = bar_heights[start];
     let i = start - 1;
     let j = end + 1;
@@ -121,11 +160,11 @@ async function partition_hoare(start, end) {
     }
 }
 
-async function quick_sort_hoare(start = 0, end = num_bars - 1) {
+async function quick_sort(start = 0, end = num_bars - 1) {
     if (start < end) {
-        let partn_idx = await partition_hoare(start, end);
-        await quick_sort_hoare(start, partn_idx);
-        await quick_sort_hoare(partn_idx + 1, end);
+        let partn_idx = await partition(start, end);
+        await quick_sort(start, partn_idx);
+        await quick_sort(partn_idx + 1, end);
     }
 }
 
@@ -136,25 +175,30 @@ async function quick_sort_hoare(start = 0, end = num_bars - 1) {
 // Thank you, Rabbid76.
 
 async function merge_sort() {
-    arr_copy = bar_heights.slice();
-    await merge_sort_slice(arr_copy, 0, num_bars);
+    let arr_copy = bar_heights.slice();
+    let color_arr_copy = bar_colors.slice();
+    await merge_sort_slice(arr_copy, color_arr_copy, 0, arr_copy.length);
+    return;
 }
 
-async function merge_sort_slice(arr_copy, start, end) {
+async function merge_sort_slice(arr, color_arr, start, end) {
     if (end - start <= 1)
         return;
     let mid = Math.round((end + start) / 2);
-    await merge_sort_slice(arr_copy, start, mid);
-    await merge_sort_slice(arr_copy, mid, end);
+    await merge_sort_slice(arr, color_arr, start, mid);
+    await merge_sort_slice(arr, color_arr, mid, end);
     let i = start;
     let j = mid;
     while (i < end && j < end) {
-        if (arr_copy[i] > arr_copy[j]) {
-            let t = arr_copy[j];
+        if (arr[i] > arr[j]) {
+            let t1 = arr[j];
+            let t2 = color_arr[j];
             apply_colors(i, end, bar_color_width, 'red', 'merge');
             apply_colors(j, end, bar_color_width, 'red', 'merge');
-            arr_copy.splice(j, 1);
-            arr_copy.splice(i, 0, t);
+            arr.splice(j, 1);
+            color_arr.splice(j, 1);
+            arr.splice(i, 0, t1);
+            color_arr.splice(i, 0, t2);
             await sleep(20);
             apply_colors(i, end, bar_color_width, 'white', 'merge');
             apply_colors(j, end, bar_color_width, 'white', 'merge');
@@ -164,6 +208,7 @@ async function merge_sort_slice(arr_copy, start, end) {
         if (i == j)
             j++;
         // copy back the current state of the sorting
-        bar_heights = arr_copy.slice();
+        bar_heights = arr.slice();
+        bar_colors = color_arr.slice();
     }
 }
--- sorting-visualizer/js/sorting-visualizer.js
+++ sorting-visualizer/js/sorting-visualizer.js
@@ -1,30 +1,46 @@
-const margin_top_percentage = 0.5;
-const bar_color = 255;
-const bar_colors = [];
-const available_sorts = {
-    'bubble': bubble_sort,
-    'selection': selection_sort,
-    'quicksort_l': quick_sort_lomuto,
-    'quicksort_h': quick_sort_hoare,
-    'insertion': insertion_sort,
-    'merge': merge_sort
-}
+const margin_top_percentage = 0.5,
+    bar_color = 255,
+    available_sorts = {
+        'bubble': bubble_sort,
+        'cocktail': cocktail_shaker_sort,
+        'selection': selection_sort,
+        'insertion': insertion_sort,
+        'shell': shell_sort,
+        'quicksort': quick_sort,
+        'merge': merge_sort
+    };
 
-let num_bars = 200;
-let bar_width;
-let bar_color_width = 2;
-let currently_sorting = false;
-let stopped = false;
-let selected_sort = 'bubble';
-let bar_heights = []; // Didn't make this const since merge sort needs the array to be reassignable
+let colored_bars = true,
+    num_bars = 150,
+    bar_width,
+    bar_color_width = 2,
+    currently_sorting = false,
+    stopped = false,
+    selected_sort = 'bubble',
+    bar_heights = [], // Didn't make this const since merge sort needs the array to be reassignable
+    bar_colors = [], // same
+    color_gradients = [],
+    random_color_index,
+    retrieved_gradients = true;
 
 function setup() {
     frameRate(60);
     createCanvas(windowWidth, windowHeight);
     bar_width = windowWidth / num_bars;
-    generate_heights();
-    draw_bars();
-    noLoop();
+    axios.get('https://raw.githubusercontent.com/ghosh/uiGradients/master/gradients.json')
+        .then(function(response) {
+            color_gradients = response.data;
+            random_color_index = Math.floor(Math.random() * color_gradients.length + 1);
+        })
+        .catch(function(error) {
+            console.log(error);
+            retrieved_gradients = false;
+        })
+        .then(function() {
+            generate_heights_and_color_in();
+            draw_bars();
+            noLoop();
+        });
 }
 
 function draw() {
@@ -40,12 +56,34 @@ function draw_bars() {
     }
 }
 
-function generate_heights() {
-    bar_heights.splice(0, bar_heights.length); // empties array
-    for (let i = 0; i < num_bars; i++) {
+function generate_heights_and_color_in() {
+    bar_heights.splice(0, bar_heights.length);
+    for (let i = 0; i < num_bars; i++)
         bar_heights.push(Math.floor(Math.random() * (windowHeight - (windowHeight * margin_top_percentage))));
-        bar_colors.push(color('white'));
+    set_bar_colors();
+}
+
+function set_bar_colors() {
+    bar_colors.splice(0, bar_colors.length); // empties array
+    if (colored_bars) {
+        let color_scale;
+        if (retrieved_gradients)
+            color_scale = chroma.scale(color_gradients[random_color_index].colors).domain([0, Math.max(...bar_heights)]);
+        else
+            color_scale = chroma.scale(['yellow', 'navy']).mode('lch').domain([0, Math.max(...bar_heights)]);
+        for (let i = 0; i < num_bars; i++) {
+            let new_color = color_scale(bar_heights[i]),
+                r = Math.floor(new_color._rgb[0]),
+                g = Math.floor(new_color._rgb[1]),
+                b = Math.floor(new_color._rgb[2]);
+            bar_colors.push(color(r, g, b));
+        }
+    } else {
+        for (let i = 0; i < num_bars; i++)
+            bar_colors.push(color('white'));
     }
+    loop();
+    noLoop();
 }
 
 function windowResized() {
@@ -56,9 +94,7 @@ function redraw_bars(resize = false) {
     if (!currently_sorting) {
         $('#start-button').removeAttr('disabled');
         bar_width = windowWidth / num_bars;
-        loop();
-        generate_heights();
-        noLoop();
+        generate_heights_and_color_in();
         if (resize)
             resizeCanvas(windowWidth, windowHeight);
     }
@@ -69,27 +105,37 @@ $(document).ready(function() {
 });
 
 function initialize() {
-    $('#start-button').on('click', async function() {
-        $('#start-button, #num-bars-range, #new-button, #sort-select-dropdown').attr('disabled', true);
-        $('#reset-button').removeAttr('disabled');
-        currently_sorting = true;
-        loop();
-        await available_sorts[selected_sort]();
-        noLoop();
-        currently_sorting = false;
-        $('#sort-select-dropdown, #new-button, #num-bars-range').removeAttr('disabled');
-    });
-
-    $('#new-button').on('click', () => redraw_bars());
-
     $('#num-bars-range').on('input', function() {
         num_bars = $(this).val();
         redraw_bars();
     });
 
     $('#sort-select-dropdown').on('change', function() {
-        selected_sort = $(this).val()
+        selected_sort = $(this).val();
     });
 
+    $('#color-toggle-switch').on('change', function() {
+        $('#color-toggle-switch').is(':checked') ? colored_bars = true : colored_bars = false;
+        set_bar_colors();
+    });
+
+    $('#start-button').on('click', async function() {
+        $('#num-bars-range, #color-toggle-switch, #sort-select-dropdown, #start-button, #new-button, #new-colors-button').attr('disabled', true);
+        $('#reset-button').removeAttr('disabled');
+        currently_sorting = true;
+        loop();
+        await available_sorts[selected_sort]();
+        noLoop();
+        currently_sorting = false;
+        $('#num-bars-range, #color-toggle-switch, #sort-select-dropdown, #new-button, #new-colors-button').removeAttr('disabled');
+    });
+
+    $('#new-button').on('click', () => redraw_bars(resize = true));
+
     $('#reset-button').on('click', () => window.location.reload());
+
+    $('#new-colors-button').on('click', () => {
+        random_color_index = Math.floor(Math.random() * color_gradients.length + 1);
+        set_bar_colors();
+    });
 }
